480d35affe6f88256b26ac795cbe682f0d8f2a4f
Gabriel Genê <gabrielgene97@gmail.com>
2018-02-17 13:12:05 -0300

    Improve task logic

--- cron/task.js
+++ cron/task.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 const amqp = require('amqplib/callback_api');
+const mongoose = require('mongoose');
+const Profile = require('../models/profile');
 
 const defaultMessage = `{
   "user": "managerinsta97",
@@ -11,16 +13,26 @@ const defaultMessage = `{
   "profile_list": ["marvel"]
 }`;
 
-const task = (msg = defaultMessage) => {
-  amqp.connect('amqp://127.0.0.1', function (err, conn) {
-    conn.createChannel(function (err, ch) {
-      const q = 'task_queue';
+const task = () => {
+  mongoose.connect('mongodb://172.17.0.4:27017/admin', (err, res) => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+  });
 
-      ch.assertQueue(q, { durable: true });
-      ch.sendToQueue(q, new Buffer(msg), { persistent: true });
-      console.log(" [x] Sent '%s'", msg);
-    });
-    setTimeout(function () { conn.close(); }, 500);
+  Profile.find({}, (err, profiles) => {
+    profiles.forEach(profile => {
+      amqp.connect('amqp://127.0.0.1', function (err, conn) {
+        conn.createChannel(function (err, ch) {
+          const q = 'task_queue';
+          const msg = JSON.stringify(profile);
+
+          ch.assertQueue(q, { durable: true });
+          ch.sendToQueue(q, new Buffer(msg), { persistent: true });
+          console.log(" [x] Sent '%s'", msg);
+        });
+        setTimeout(function () { conn.close(); }, 500);
+      });
+    })
   });
 }
 
