5906b743b8107a35c033b061b4223aab65a28c2e
Gabriel Genê <gabrielgene97@gmail.com>
2018-02-24 11:32:16 -0300

    Create login fetch

---
+++ client/src/fetches/index.js
@@ -0,0 +1,22 @@
+const handleHttpStatus = res => {
+  if (res.status >= 200 && res.status < 300) {
+    return res.json();
+  }
+  throw res;
+};
+
+const createErrorHandler = defaultValue => res => {
+  console.error('request failed', res);
+  return defaultValue;
+};
+
+export const postLogin = (login) => {
+  return fetch('/api/login', {
+    credentials: 'same-origin',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(login),
+  }).then( res => res.status).catch(createErrorHandler(500));
+}
