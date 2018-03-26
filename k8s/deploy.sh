f55fc952422375434ad96a9e14077ded824c319f
Gabriel Genê <gabrielgene97@gmail.com>
2018-03-25 23:27:23 -0300

    Change k8s deployments

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 NAME=insta-js YARN_ARG=start TAG=$(git rev-parse HEAD) ./k8s/template.sh | kubectl apply -f -
-# NAME=cron-js YARN_ARG=cron TAG=$(git rev-parse HEAD) ./k8s/template.sh | kubectl apply -f -
-# NAME=worker-js YARN_ARG=worker TAG=$(git rev-parse HEAD) ./k8s/template.sh | kubectl apply -f -
+NAME=cron-js YARN_ARG=cron TAG=$(git rev-parse HEAD) ./k8s/template.sh | kubectl apply -f -
+NAME=worker-js YARN_ARG=worker TAG=$(git rev-parse HEAD) ./k8s/template.sh | kubectl apply -f -
 kubectl apply -f ./k8s/service.yaml
