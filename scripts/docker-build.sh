6112578a6563d4bc283c07f388b56aafeef761d2
Gabriel Genê <gabrielgene97@gmail.com>
2018-04-12 15:10:01 -0300

    change build scritps

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,2 @@
 docker build -t gabrielgene/insta-js .
 docker tag gabrielgene/insta-js gabrielgene/insta-js:$(git rev-parse HEAD)
-docker tag gabrielgene/insta-js gabrielgene/insta-js:local3
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,3 +1,2 @@
 docker push gabrielgene/insta-js
 docker push gabrielgene/insta-js:$(git rev-parse HEAD)
-docker push gabrielgene/insta-js:local3
